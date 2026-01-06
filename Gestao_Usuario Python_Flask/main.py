017274ce3801c563f7a8e082677d887ff0eeb29c
Hugo Kokay <hugokoka@gmail.com>
2026-01-06 15:47:11 -0300

    Atualização do código Flask

--- Gestao_Usuario Python_Flask/main.py
+++ Gestao_Usuario Python_Flask/main.py
@@ -1,3 +1,12 @@
-From flask import Flask
+from flask import Flask
 
 # inicialização do aplicativo Flask
+app = Flask(__name__)
+
+#rotas
+@app.route('/') 
+def ola_mundo():
+    return "Olá, Mundo!"
+
+# execução do aplicativo
+app.run (debug=True) # modo de depuração ativado
